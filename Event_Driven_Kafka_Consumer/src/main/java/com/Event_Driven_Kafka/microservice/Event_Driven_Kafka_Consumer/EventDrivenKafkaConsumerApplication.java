e7f05a14b470e639cecdce24d5559ccc41f4c331
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2024-08-11 23:29:30 +0530

    Add files via upload
    Added Kafka Consumer for retrieving user object

---
+++ Event_Driven_Kafka_Consumer/src/main/java/com/Event_Driven_Kafka/microservice/Event_Driven_Kafka_Consumer/EventDrivenKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.Event_Driven_Kafka.microservice.Event_Driven_Kafka_Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ Event_Driven_Kafka_Consumer/src/test/java/com/Event_Driven_Kafka/microservice/Event_Driven_Kafka_Consumer/EventDrivenKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Event_Driven_Kafka.microservice.Event_Driven_Kafka_Consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
