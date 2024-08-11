70f19ee07ff1d29d37590b668c979eb0afaaae04
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2024-08-11 22:24:04 +0530

    Add files via upload
    Added skeleton for Kafka consumer for user

---
+++ Event_Driven_Kafka/src/main/java/com/Event_Driven_Kafka/microservice/Event_Driven_Kafka/EventDrivenKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.Event_Driven_Kafka.microservice.Event_Driven_Kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenKafkaApplication.class, args);
+	}
+
+}
---
+++ Event_Driven_Kafka/src/test/java/com/Event_Driven_Kafka/microservice/Event_Driven_Kafka/EventDrivenKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Event_Driven_Kafka.microservice.Event_Driven_Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
